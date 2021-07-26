20f879a7a67c21f2a378cbae1b304a51415816ed
Kartik Tripathi <kartiktripathi.1000@gmail.com>
2021-07-26 16:17:29 +0530

    prefixes change added

--- Code_discord.py
+++ Code_discord.py
@@ -9,24 +9,59 @@ import discord
 import logging
 from discord.ext import commands
 
+'''
 # importing logger and loging all the process of bot in a text file.
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
+'''
 
 
 #making bot (intent are the previlage esclation for the bot so that it can access the member lisst or the the gluid)
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
-client = commands.Bot(command_prefix = '.', intents = intents)
+
+def get_prefix(client,message):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+        return prefixes[str(message.guild.id)]
+
+client = commands.Bot(command_prefix = get_prefix, intents = intents)
 
 # assigning tasks to the bot.
 
 @client.event
 async def on_ready():
     print("Bot's up and running")
+
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+        prefixes[str(guild.id)] = '.'
+        with open('prefixes.json','w') as f:
+            json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+        prefixes.pop(str(guild.id))
+        with open('prefixes.json','w') as f:
+            json.dump(prefixes,f,indent=4)
+
+@client.command()
+async def change_prefix(ctx, prefix):
+    with open('prefixes.json','r') as f:
+        prefixes = json.load(f)
+        prefixes[str(ctx.guild.id)] = prefix
+        with open('prefixes.json','w') as f:
+            json.dump(prefixes, f, indent = 4)
+            await ctx.send(f'prefix changed to {prefix}') 
+
+
+
 
 @client.command()
 async def ping(ctx):
@@ -39,6 +74,8 @@ async def on_member_join(member):
 @client.event
 async def on_member_remove(member):
     print(f'{member} has been removed')
+
+
 
 # calling bot
 client.run('ODU4NjA0NDk3NzY4MDIyMDI2.YNgjwA.ir62cwQsbo10p0pkHGMoK4OLWz8')
