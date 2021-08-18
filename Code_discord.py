3211adfdf4af4527ad207aa7bd0d7319ed8fdb7b
Kartik Tripathi <kartiktripathi.1000@gmail.com>
2021-08-18 22:03:19 +0530

    arranged the commands more professionally

--- Code_discord.py
+++ Code_discord.py
@@ -8,7 +8,8 @@ kartik tripathi
 import discord
 import logging
 import json
-import random
+import Music
+import Basic_functionalities
 from discord import user
 from discord.ext import commands
 
@@ -69,69 +70,7 @@ async def change_prefix(ctx, prefix):
             json.dump(prefixes, f, indent = 4)
             await ctx.send(f'prefix changed to {prefix}') 
 
-#assigning task
 
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'pong!{round(client.latency*1000)}ms')
-
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined')
-    
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has been removed')
-
-@client.command()
-async def clear(ctx,amount=5):
-    await ctx.channel.purge(limit=amount)
-
-@client.command()
-async def ban(ctx,member:discord.member,*,reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
-
-@client.command()
-async def unban(ctx,*,member):
-    banned_user = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-    for ban_entry in banned_users:
-        user = ban_entry.user
-        if(user.name, user.discriminator)==(member_name,-member.discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned{user.mention}')
-            return
-
-@client.command(alias = ['8 ball','test'])
-async def _8ball(ctx,*,question):
-    responces = ["It is certain.",
-                "It is decidedly so.",
-                "Without a doubt.",
-                "Yes - definitely.",
-                "You may rely on it.",
-                "As I see it, yes.",
-                "Most likely.",
-                "Outlook good.",
-                "Yes.",
-                "Signs point to yes.",
-                "Reply hazy, try again.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don't count on it.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Very doubtful."]
-    await ctx.send(f'Question:{question}\n Answer: {random.choice(responces)}')
-
-@client.command
-async def connect(* reconnect ,timeout = 10):
-    await ctx.send('the bot is connected')
-
-#vv
 # calling bot
 client.run('ODU4NjA0NDk3NzY4MDIyMDI2.YNgjwA.ir62cwQsbo10p0pkHGMoK4OLWz8')
--- Music.py
+++ Music.py
@@ -7,6 +7,3 @@ from discord.ext import commands
 async def connect(* reconnect ,timeout = 10):
 await voice_client()
     await ctx.send('the bot is connected')
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'pong!{round(client.latency*1000)}ms')
