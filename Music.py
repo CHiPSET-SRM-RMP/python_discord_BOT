31a828f746275055d28d932f26ab3c32f8e0e3ad
Kartik Tripathi <kartiktripathi.1000@gmail.com>
2021-08-18 22:28:16 +0530

    small fix

--- Music.py
+++ Music.py
@@ -1,10 +1,10 @@
 #music functionality
 import discord
 from discord import voice_client
-from discord.client import Client
+from discord.client import client
 from discord.ext import commands
 
-@Client.command()
+@client.command()
 async def startq(ctx):
     voicechannel = discord.utils.get(ctx.guild.channels, name='queue')
     vc = await voicechannel.connect()
