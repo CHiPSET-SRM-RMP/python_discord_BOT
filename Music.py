5e03f5d5ad3b9109039d55aef44849cb5b244c63
Kartik Tripathi <kartiktripathi.1000@gmail.com>
2021-08-20 11:56:42 +0530

    minor fix

--- Music.py
+++ Music.py
@@ -1,10 +1,12 @@
 #music functionality
 import discord
 from discord import voice_client
+from discord import client
+from discord import guild
 from discord.ext import commands
+import Code_discord
 
 @client.command()
-async def startq(ctx):
-    voicechannel = discord.utils.get(ctx.guild.channels, name='queue')
-    vc = await voicechannel.connect()
-    vc.play(discord.FFmpegPCMAudio("countdown.wav"), after=lambda e: print('done', e))
+async def join(ctx):
+    await voice_client.VoiceClient.connect(self, reconnect= True, timeout= None)
+    await guild.Guild.change_voice_state(self, channel= '655484906331176970', self_mute= False, self_deaf= False)
